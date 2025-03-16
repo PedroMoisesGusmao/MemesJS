66af784d6f75861d46488b25f6162e4e5d96e1b9
Vigamy <andreotti.will@gmail.com>
2025-03-16 20:51:35 -0300

    Adding main design functions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import MemeCarousel from "../components/MemeCarousel";
+import MemeCarousel from "../components/memeCarousel/MemeCarousel";
 
 export default function Home() {
   return (
