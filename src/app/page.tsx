76c42197f99b33e213960a149300fcba7bf9d910
Pedro <pedroaraujo4910@gmail.com>
2025-04-02 22:20:09 -0300

    Fixing not invisible page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import MemeCarousel from "../components/memeCarousel/MemeCarousel";
 
-export default async function Home() {
-  <MemeCarousel/>
+export default function Home() {
+  return <MemeCarousel/>
 }
